b8c11dbe9cc5cf71d19f9cc668ffbf1f11fd9b17
Ethan Ruiz <etanr25@gmail.com>
2021-07-26 19:56:10 -0600

    v.011

--- JS/menu.js
+++ JS/menu.js
@@ -2,13 +2,12 @@ let menuToggle = document.querySelector(".header__menu-toggle");
 let menuToggleIcon = document.querySelector(".header__menu-toggle i");
 let menu = document.querySelector(".header__navbar");
 
-  menuToggle.addEventListener("click", e=> {
+  menuToggle.addEventListener("click", ()=> {
     menu.classList.toggle("active");
-
+    
     if(menu.classList.contains("active")){
       menuToggleIcon.setAttribute("class", "fa fa-times");  
     } else{
       menuToggleIcon.setAttribute("class","fa fa-bars");
     }
-
   });
